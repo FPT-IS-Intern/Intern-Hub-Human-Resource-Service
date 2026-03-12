46623b58b4f701c2e20e2778f1092148295c392a
GWUY <buigiahuy1506@gmail.com>
2026-03-12 07:42:27 +0700

    change method in lock and unlock account

--- infra/src/main/java/com/fis/hrmservice/infra/feign/client/AuthIdentityFeignClient.java
+++ infra/src/main/java/com/fis/hrmservice/infra/feign/client/AuthIdentityFeignClient.java
@@ -7,10 +7,7 @@ import com.fis.hrmservice.infra.feign.response.UserRoleInfraResponse;
 import com.fis.hrmservice.infra.model.CreateUserPassIdentityRequest;
 import com.intern.hub.library.common.dto.ResponseApi;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,10 +17,10 @@ public interface AuthIdentityFeignClient {
   @PostMapping("/auth/internal/identities/userpass")
   void createAuthIdentity(@RequestBody CreateUserPassIdentityRequest request);
 
-  @PostMapping("/auth/internal/identity/lock/{userId}")
+  @PutMapping("/auth/internal/identity/lock/{userId}")
   void lockAuthIdentity(@PathVariable Long userId);
 
-  @PostMapping("/auth/internal/identity/un-lock/{userId}")
+  @PutMapping("/auth/internal/identity/un-lock/{userId}")
   void unlockAuthIdentity(@PathVariable Long userId);
 
   @GetMapping("/auth/authz/roles/by-user/{userId}")
