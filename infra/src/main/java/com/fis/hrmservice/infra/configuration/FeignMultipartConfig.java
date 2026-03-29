ce08b34c029206f6d8f694cbd6c2becd1bc56c32
GWUY <buigiahuy1506@gmail.com>
2026-03-29 21:47:38 +0700

    fix: scope FeignMultipartConfig to TicketFeignClient only

--- infra/src/main/java/com/fis/hrmservice/infra/configuration/FeignMultipartConfig.java
+++ infra/src/main/java/com/fis/hrmservice/infra/configuration/FeignMultipartConfig.java
@@ -3,9 +3,7 @@ package com.fis.hrmservice.infra.configuration;
 import feign.codec.Encoder;
 import feign.form.spring.SpringFormEncoder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 public class FeignMultipartConfig {
 
     @Bean
