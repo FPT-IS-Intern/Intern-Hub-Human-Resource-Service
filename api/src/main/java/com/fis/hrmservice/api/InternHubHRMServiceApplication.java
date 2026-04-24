401887250de330948588af4862e4f8bd9972cd26
Bravos <baonq8205@gmail.com>
2026-04-24 13:27:08 +0700

    feat(database): configure Liquibase and add initial changelog files

--- api/src/main/java/com/fis/hrmservice/api/InternHubHRMServiceApplication.java
+++ api/src/main/java/com/fis/hrmservice/api/InternHubHRMServiceApplication.java
@@ -5,10 +5,13 @@ import com.intern.hub.starter.security.annotation.EnableSecurity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(scanBasePackages = "com.fis.hrmservice")
+@SpringBootApplication(scanBasePackages = "com.fis.hrmservice", exclude = {
+    UserDetailsServiceAutoConfiguration.class
+})
 @EnableJpaRepositories(basePackages = "com.fis.hrmservice.infra.persistence.repository")
 @EntityScan(basePackages = "com.fis.hrmservice.infra.persistence.entity")
 @EnableGlobalExceptionHandler
